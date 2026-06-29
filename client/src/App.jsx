2831eac27daa91d9ad22f102776c13d2233975f1
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2026-06-29 16:52:34 +0530

    add the toast position and define in globally

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Toaster } from 'react-hot-toast';
 import { AuthProvider } from './context/AuthContext';
 import AppRouter from './routes/AppRouter';
 import useGetMe from './hooks/auth/useGetMe';
@@ -42,6 +43,7 @@ function App() {
       <AuthProvider>
         <RootApp />
       </AuthProvider>
+      <Toaster position="top-center" />
       <ReactQueryDevtools initialIsOpen={false} position="bottom" />
     </QueryClientProvider>
   );
