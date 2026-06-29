1f7f2a8ab349c682d6e6f81436af9b706afe1163
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2026-06-29 11:32:18 +0530

    update the useGetRoles hook

--- client/src/hooks/roles/useGetRoles.js
+++ client/src/hooks/roles/useGetRoles.js
@@ -1,11 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
 import axiosInstance from '../../api/axiosInstance';
 
-const useGetRoles = () => {
+const useGetRoles = (params = {}) => {
   return useQuery({
-    queryKey: ['roles'],
+    queryKey: ['roles', params],
     queryFn: async () => {
-      const res = await axiosInstance.get('/roles');
+      const res = await axiosInstance.get('/roles', { params });
       return res.data.roles;
     },
     staleTime: 3 * 60 * 1000,
